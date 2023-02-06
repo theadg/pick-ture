e562557916e738bbd2044e9cacca23996c1140d9
theadg <deguzmanbernardandrew@gmail.com>
2023-02-06 23:27:33 +0800

    Add steps array

---
+++ app/src/pages/Home/steps.js
@@ -0,0 +1,26 @@
+import heart from '../../assets/heart.svg';
+import explore from '../../assets/explore.svg';
+import upload from '../../assets/upload.svg';
+
+export default Array = [
+  {
+    icon: upload,
+    heading: 'Upload your Photo',
+    para: 'The best moments are the ones you capture  and share.Simply press go to the Photos tab and click upload.',
+    index: 1,
+  },
+
+  {
+    icon: heart,
+    heading: 'vote for photos',
+    para: 'The best moments are the ones you capture  and share.Simply press go to the Photos tab and click upload.',
+    index: 2,
+  },
+
+  {
+    icon: explore,
+    heading: 'explore categories',
+    para: 'The best moments are the ones you capture  and share.Simply press go to the Photos tab and click upload.',
+    index: 3,
+  },
+];
