38a8558c2075f4d56c37bada25c61df747bf99cb
theadg <deguzmanbernardandrew@gmail.com>
2023-02-07 15:21:09 +0800

    Remove old code and add footer

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,21 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'animate.css';
-
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
+import Footer from './components/Footer/Footer';
+// TODO: routes
 export default function App() {
   // console.log(faClose);
   return (
     <>
-      {/* <Test /> */}
-      {/* <AccessAlarm /> */}
       <Navbar />
       <Home />
-      {/* <FontAwesomeIcon
-        icon={faClose}
-        className={`ml-auto fa-xl ease-in-out duration-150 hover:text-primary-blue hover:cursor-pointer sm:hidden`}
-      /> */}
+      <Footer />
     </>
   );
 }
