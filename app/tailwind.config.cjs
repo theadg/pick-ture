e9f4ffcab66942800e1565249e583fff98e70f78
theadg <deguzmanbernardandrew@gmail.com>
2023-02-07 14:26:48 +0800

    Update configs

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -6,11 +6,16 @@ module.exports = {
       main: ['Unbounded'],
       sans: ['Quicksand', 'sans-serifs'],
     },
-    // TODO: Add Color Palette
+    maxWidth: {
+      xs: '20rem',
+      sm: '24rem',
+      desk: '800px',
+    },
     colors: {
       'primary-blue': '#00ADB5',
       'primary-bg': '#222831',
       'secondary-bg': '#393E46',
+      rose: 'rgb(136 19 55)',
       white: '#EEEEEE',
     },
     extend: {},
