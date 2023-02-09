4381070d3f34033c0c2d21c36ee5bc4da9abeb56
theadg <deguzmanbernardandrew@gmail.com>
2023-02-09 21:41:12 +0800

    Remove unnecessary code

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,22 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'animate.css';
-import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
-import Footer from './components/Footer/Footer';
 import Photos from './pages/Photos/Photos';
 import Explore from './pages/Explore/Explore';
+import Category from './pages/Category/Category';
 import { Route, Routes } from 'react-router-dom';
 
 // TODO: routes
 export default function App() {
-  // console.log(faClose);
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/photos" element={<Photos />} />
         <Route path="/explore" element={<Explore />} />
+        <Route path="/category/:title" element={<Category />} />
       </Routes>
     </>
   );
