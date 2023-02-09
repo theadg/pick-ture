4fb833589dc570514a4cc8e1392943a93b3bde14
theadg <deguzmanbernardandrew@gmail.com>
2023-02-09 21:42:54 +0800

    Rearrange categories

--- app/src/pages/Explore/categories.js
+++ app/src/pages/Explore/categories.js
@@ -29,4 +29,38 @@ export const categories = [
     image:
       'https://images.unsplash.com/photo-1489278353717-f64c6ee8a4d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
   },
+  {
+    title: 'renders',
+    image:
+      'https://images.unsplash.com/photo-1675050757468-093be55c00fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+  {
+    title: 'art',
+    image:
+      'https://images.unsplash.com/photo-1675416084816-72a68b2a0497?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+  {
+    title: 'pattern',
+    image:
+      'https://images.unsplash.com/photo-1675317859828-7cd2bb427a0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
+  },
+  {
+    title: 'design',
+    image:
+      'https://images.unsplash.com/photo-1675025679348-5906632eb467?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1205&q=80',
+  },
+  {
+    title: 'fashion',
+    image:
+      'https://images.unsplash.com/photo-1675101085697-148c00a92a00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+  },
+  {
+    title: 'film',
+    image:
+      'https://images.unsplash.com/photo-1647089490798-573b8b47ad9b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2075&q=80',
+  },
+  // {
+  //   title: '',
+  //   image: '',
+  // },
 ];
