54607b798ece04bf110d7f63ad6333ea6cc27b6c
theadg <deguzmanbernardandrew@gmail.com>
2023-02-12 09:27:12 +0800

    Add error handler middleware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,15 @@
+// Middleware are the functions that run when you make a request/response
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+
+  res.status(statusCode);
+
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+};
+
+module.exports = {
+  errorHandler,
+};
