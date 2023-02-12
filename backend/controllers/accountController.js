2687aa7afa21f34d55f9270270ab6014476b9780
theadg <deguzmanbernardandrew@gmail.com>
2023-02-12 09:37:13 +0800

    Add asyncHandler

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -1,24 +1,32 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc    Get Goals
 // @route   GET /signup
 // access   Public
-const getAccounts = (req, res) => {
+const getAccounts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Accounts got got!' });
-};
+});
 
 // @desc    PUT Goals
-// @route   POST /signup
+// @route   POST /signup/:id
 // access   Public
-const createAccount = (req, res) => {
+const createAccount = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+
+    throw new Error('Please add a text field');
+  }
   res.status(200).json({ message: `Accounts created id=${req.params.id}` });
-};
+});
 
-// @desc    PUT Goals
-// @route   POST /signup
+// @desc    DELETE account
+// @route   DELETE /:id
 // access   Public
-const deleteAccount = (req, res) => {
+const deleteAccount = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Accounts deleted id=${req.params.id}` });
-};
+});
 
+// USED ASYNC FOR PROMISE
 module.exports = {
   getAccounts,
   createAccount,
