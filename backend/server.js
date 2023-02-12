9c59099fb43893ecff1fac3db5b0a03a4b5bd39c
theadg <deguzmanbernardandrew@gmail.com>
2023-02-12 09:27:03 +0800

    Add errorHandler and middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+// error handler
+const { errorHandler } = require('./middleware/errorMiddleware');
 // run server here
 const port = process.env.PORT || 5000;
 
 // initialize express
 const app = express();
+
+// allow to console.log req.body data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // before transporting to signUpRoutes:
 // app.get('/', (req, res) => {
@@ -15,4 +21,7 @@ const app = express();
 // after transporting to signUpRoutes:
 app.use('/signup', require('./routes/signUpRoutes'));
 
+// overwrite default error handler
+// from html to stack trace
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on ${port}`));
