bb5aa49c14048300b809859c48675c72331d8fa1
theadg <deguzmanbernardandrew@gmail.com>
2023-02-13 22:00:07 +0800

    Add userSchema

---
+++ backend/schema/userSchema.js
@@ -0,0 +1,26 @@
+// odm to connect to mongodb
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+    },
+  },
+  {
+    // for created at and updated at
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);
