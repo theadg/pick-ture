5553f944f8c40395f2f7bf87f98f8f920582bac1
theadg <deguzmanbernardandrew@gmail.com>
2023-02-16 15:14:53 +0800

    Add authentication

---
+++ app/src/AuthContext.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => {
+  return React.useContext(AuthContext);
+};
